bd00a23979da98975e1ca99667b107fa7ef967fb
piecyk <piecyk@gmail.com>
2011-03-13 19:46:55 +0100

    first commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Tppircms01::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tppircms01::Application.config.secret_token = '86d1b1d7b8b2c01708a1e88c368cccd0697038353bc5dc3539e45eac355a5e54a1426330f9665eb1c8b69ea5f2bd0709306912d06eb687a6284a136294e1b493'
